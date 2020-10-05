5e343ab1a648ff31724f209b4b57522fe6a0318c
KRAdev <kradevstudio@gmail.com>
2020-10-05 20:35:02 +0300

    s
    d

---
+++ build/precache-manifest.7be5bb05b4caea947e55110ce9417407.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "463c8aa6e7f60d8c295223d22928322d",
+    "url": "/index.html"
+  },
+  {
+    "revision": "123103bfd5d8c0482b9f",
+    "url": "/static/css/main.5f361e03.chunk.css"
+  },
+  {
+    "revision": "1dc60055faa2d3f597ac",
+    "url": "/static/js/2.a9b595aa.chunk.js"
+  },
+  {
+    "revision": "0749163b59fbee32225059cb60c18af6",
+    "url": "/static/js/2.a9b595aa.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "123103bfd5d8c0482b9f",
+    "url": "/static/js/main.a71efa8b.chunk.js"
+  },
+  {
+    "revision": "73635f645c271e35ff15",
+    "url": "/static/js/runtime-main.83c3e0c4.js"
+  }
+]);
